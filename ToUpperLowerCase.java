531a8f2aa8c039aa66a0c1942412f003db64c114
삼코 <96531081+kjy228@users.noreply.github.com>
2022-01-28 22:21:48 +0900

    대소문자 변환
    대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력
    (단, 문자열의 길이는 100을 넘지 않고 영어 알파벳으로만 구성)

--- ToUpperLowerCase.java
+++ ToUpperLowerCase.java
@@ -1,11 +1,12 @@
 import java.util.*;
+
 class ToUpperLowerCase {
     public String solution(String str){
         String answer="";
         for(char x : str.toCharArray()){
             if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x);
             else answer+=Character.toLowerCase(x);
-            //아스키코드 이용하는 방식
+            // 아스키코드를 이용하는 방식
 //            if(x>=97 && x<=122) answer+=(char)(x-32);
 //            else answer+=(char)(x+32);
         }
